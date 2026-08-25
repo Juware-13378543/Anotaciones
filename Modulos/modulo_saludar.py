694852c973a55b8c32e216516bcd9fe3a8026785
Juware-13378543 <bitacoradigimail.com>
2026-08-25 15:48:40 -0500

    Se ha a incrustado la seccion de modulos, donde se usa para importar y agilizar algoritmos

---
+++ Modulos/modulo_saludar.py
@@ -0,0 +1,5 @@
+def saludar(name):
+    return f"Hola {name}! Espero que hayas tenido un marivillosos dia"
+
+def saludo_raro(name):
+    return f"Konda {name} Espero que estes nashe, anasheli anasheiiii"
---
+++ Modulos/modulos.py
@@ -0,0 +1,14 @@
+#Importando un modulo y asignandole el nombre "m_saludar"
+#import modulo_saludar as m_saludar
+
+
+#desde el modulo, importamos dos funciones y les cambiamos los nombres
+from modulo_saludar import saludar as saludo_normal, saludo_raro as saludo_jajaja
+
+#creamos las variables con los saludos
+saludo_raro = saludo_jajaja("Miguel")
+saludo = saludo_normal("Julian")
+
+#Mostramos los resultados
+print(saludo)
+print(f"\n{saludo_raro}")
